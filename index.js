bfb0ef3ef042a741b8b02dbb91a990361dfa93a2
Juliana VicentimT <julianadutra1993@gmai.com>
2024-07-04 11:23:41 -0300

    adicionando elemantos JS, adicionando o botao de copiar

---
+++ index.js
@@ -0,0 +1,40 @@
+// Função para criptografar o texto
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let textoCriptografado = texto
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+  document.getElementById("mensagem").value = textoCriptografado;
+  document.getElementById("titulo mensagem").innerText =
+    "Mensagem Criptografada";
+  document.getElementById("paragrafo").innerText =
+    "Seu texto foi criptografado com sucesso!";
+}
+
+// Função para descriptografar o texto
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let textoDescriptografado = texto
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+  document.getElementById("mensagem").value = textoDescriptografado;
+  document.getElementById("titulo mensagem").innerText =
+    "Mensagem Descriptografada";
+  document.getElementById("paragrafo").innerText =
+    "Seu texto foi descriptografado com sucesso!";
+}
+
+// Função para copiar o texto
+function copiarTexto() {
+  let mensagem = document.getElementById("mensagem");
+  mensagem.select();
+  mensagem.setSelectionRange(0, 99999); // Para dispositivos móveis
+  document.execCommand("copy");
+  alert("Texto copiado para a área de transferência!");
+}
